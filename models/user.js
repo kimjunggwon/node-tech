e741732723e5b10f7c258a79b95ad55e777f20c5
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-02-05 22:28:01 +0900

    database_setting1

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const Sequelize = require('sequelize');
+
+module.export = class User extends Sequelize.Model {
+    static init(sequelize){
+        return super.init({
+            user_id: {
+                type: Sequelize.STRING(60),
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: Sequelize.STRING(100),
+                allowNull: false,
+            },
+            name: {
+                type: Sequelize.STRING(30),
+                allowNull: false,
+            },
+            email: {
+                type: Sequelize.STRING(40),
+                allowNull: false,
+                unique: true
+            },
+            birth: {
+                type: Sequelize.STRING(30),
+                allowNull: false,
+            },
+            permission: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+        }, {
+            sequelize,
+            timestamps: true,
+            paranoid: true,
+            modelName: 'User',
+            tableName: 'users',
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
+    }
+    static associate(db){}
+};
